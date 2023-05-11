3a86837ea6a4cf4dec467c61f2108320191a4858
kosei28 <niwa.kosei@gmail.com>
2023-05-11 23:55:13 +0900

    add providers index

---
+++ dist/providers/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GoogleProvider = void 0;
+var google_1 = require("./google");
+Object.defineProperty(exports, "GoogleProvider", { enumerable: true, get: function () { return google_1.GoogleProvider; } });
+//# sourceMappingURL=index.js.map
